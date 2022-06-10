91fa411bd98ef30f3d827d23458478dcc0e00db4
Joon Ho Lee <jh.lee@spira-t.com>
2022-06-10 15:42:19 +0900

    state action

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import Router from './Router';
+import { GlobalStyle } from './styles/reset';
+import { darkTheme, lightTheme } from './styles/theme';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { HelmetProvider } from 'react-helmet-async';
+import { StateCarrier } from './apis/UserTypes';
 
+const queryClient = new QueryClient()
+export const themeCarrier = new StateCarrier<boolean>()
 function App() {
+  const [mode, setMode] = useState<boolean>(true)
+  themeCarrier.addStateAction(setMode)
   return (
-    <div>Hello</div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={mode ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <HelmetProvider>
+          <Router />
+        </HelmetProvider>
+        
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>       
+    </QueryClientProvider>
+    
   );
 }
 
