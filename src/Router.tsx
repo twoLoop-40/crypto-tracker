cf58e0e22af77b1a413645fd16fb04b4c6479930
Joon Ho Lee <jh.lee@spira-t.com>
2022-06-11 00:12:57 +0900

    deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,7 @@ import { Price } from "./routes/Price"
 function Router () {
 	return <BrowserRouter>
 		<Routes>
-			<Route path="/" element={<Coins />} />
+			<Route path={process.env.PUBLIC_URL+"/"} element={<Coins />} />
 			<Route path=":coinId" element={<Coin />}>
 				<Route path="chart" element={<Chart />} />
 				<Route path="price" element={<Price />} />
